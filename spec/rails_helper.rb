bc11e23ed409cb4425b15a42362514fd1aa0b3cf
thesuss <lullisuss@gmail.com>
2016-09-20 08:14:21 +0200

    added json support file and included it in rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,8 +4,12 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 ActiveRecord::Migration.maintain_test_schema!
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
+  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
+  config.include FactoryGirl::Syntax::Methods
+  config.include ResponseJSON
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
